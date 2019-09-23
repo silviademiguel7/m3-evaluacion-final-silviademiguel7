b333d09097e95169e9118f0f577636874380deaa
silvia <silviademiguel7@gmail.com>
2019-09-23 08:29:15 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component{
+  render(){
+    return (
+      <div className="app">
+      
+      </div>
+    );
+  }
+  
+}
+
+export default App;
